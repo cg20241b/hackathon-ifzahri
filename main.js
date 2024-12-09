f2d966fa9489244f9b5ff2ea0a00b3a6e0c1ccae
Iftala <circlebytes.iftala@gmail.com>
2024-12-09 14:45:36 +0700

    feat: add threejs boilerplate

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+
+// Init
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// Webgl render
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Add box and camera
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+
+// Animation loop
+var animate = function () {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+
+// Animation
+animate();
